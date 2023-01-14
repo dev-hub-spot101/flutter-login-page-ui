fdd9eef24a384b5994a0f763e0081c529bbb363f
sudarshan101 <sudarshan.vis101@gmail.com>
2023-01-14 19:54:42 +0530

    login page ui

---
+++ lib/widgets/fadeanimation.dart
@@ -0,0 +1,16 @@
+// import 'package:flutter/material.dart';
+// import 'package:simple_animations/simple_animations.dart';
+//
+//
+// class FadeAnimation extends StatelessWidget {
+//   final double delay;
+//   final Widget child;
+//   const FadeAnimation({Key? key, required this.delay, required this.child}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     // final tween  = MultiTrackTween([
+//     //
+//     // ])
+//   }
+// }
